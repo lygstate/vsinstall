48d04e2ca78ae89e03488169dc7eb466ef5b5f33
Yonggang Luo <luoyonggang@gmail.com>
2015-03-14 15:12:45 +0800

    Install runtime things.

--- install_runtime.bat
+++ install_runtime.bat
@@ -1,10 +1,11 @@
-cd /d %~dp0
+cd /d %~dp0..
+set "VSDIR=%CD%"
 
-cd /d "vs2010 Debug Runtime"
+cd /d "%VSDIR%\vs2010 Debug Runtime"
 copy /Y "x64\*.dll" %WinDir%\System32\
 copy /Y "x86\*.dll" %WinDir%\SysWOW64\
 
-cd /d %~dp0
+cd /d "%VSDIR%\"
 "vs2008 Debug Runtime\VC_x86Runtime.exe" /q /norestart
 "vs2008 Debug Runtime\VC_x64Runtime.exe" /q /norestart
 
