9f51a99047b43a312cf73b0a57920a7de6ef1941
Yonggang Luo <luoyonggang@gmail.com>
2016-10-18 20:40:51 +0800

    Install VC6 properly.

--- vs6-install.bat
+++ vs6-install.bat
@@ -1,6 +1,8 @@
 @echo on
 cd /d %~dp0..
-set "VSDIR=%CD%"
+xcopy /I /C /E /Y "Microsoft Visual Studio" "%ProgramFiles(x86)%\Microsoft Visual Studio"
+
+set "VSDIR=C:\Program Files (x86)"
 
 cd /d %~dp0
 set VSDIR_REG=%VSDIR:\=\\%
