b68b51b7a683d6e77f5078e0fe90c419d4c42807
Yonggang Luo <luoyonggang@gmail.com>
2017-05-07 02:39:11 +0800

    Installing more.

--- vs2010 assembly-install.bat
+++ vs2010 assembly-install.bat
@@ -15,25 +15,36 @@ regedit.exe -s "MicrosoftSDKs registry-install.reg"
 "%SYSTEM_DIR_32%\regedit.exe" -s "vs2010 registry-install.reg"
 
 del *.reg
+cd /d "%VSDIR%\Microsoft Visual Studio 10.0\Common7\IDE\PublicAssemblies\"
+call :installAll
+cd /d "%VSDIR%\Microsoft Visual Studio 10.0\Common7\Tools\ProjectComponents\"
+call :installAll
 
-gacutil -f -i "%VSDIR%\Microsoft Visual Studio 10.0\Common7\IDE\PublicAssemblies\Microsoft.VisualStudio.VCProjectEngine.dll"
-gacutil -f -i "%VSDIR%\Microsoft Visual Studio 10.0\Common7\Tools\ProjectComponents\Microsoft.VisualStudio.ComponentModelHost.dll"
-gacutil -f -i "%VSDIR%\Microsoft Visual Studio 10.0\Common7\Tools\ProjectComponents\Microsoft.VisualStudio.Project.Contracts.dll"
-gacutil -f -i "%VSDIR%\Microsoft Visual Studio 10.0\Common7\Tools\ProjectComponents\Microsoft.VisualStudio.Project.Contracts.Implementation.dll"
-gacutil -f -i "%VSDIR%\Microsoft Visual Studio 10.0\Common7\Tools\ProjectComponents\Microsoft.VisualStudio.Project.Framework.dll"
-gacutil -f -i "%VSDIR%\Microsoft Visual Studio 10.0\Common7\Tools\ProjectComponents\Microsoft.VisualStudio.Project.Framework.Implementation.dll"
-gacutil -f -i "%VSDIR%\Microsoft Visual Studio 10.0\Common7\Tools\ProjectComponents\Microsoft.VisualStudio.Project.Utilities.v10.0.dll"
-gacutil -f -i "%VSDIR%\Microsoft Visual Studio 10.0\Common7\Tools\ProjectComponents\Microsoft.VisualStudio.Project.VisualC.VCProjectEngine.dll"
-gacutil -f -i "%VSDIR%\Microsoft Visual Studio 10.0\VC\bin\amd64\XDCMake.exe"
-gacutil -f -i "%VSDIR%\Microsoft Visual Studio 10.0\VC\bin\xdcmake.exe"
-gacutil -f -i "%VSDIR%\Microsoft SDKs\Windows\v7.1\Setup\SDKSetup.exe
+call :installSingle "%VSDIR%\Microsoft Visual Studio 10.0\VC\bin\amd64\XDCMake.exe"
+call :installSingle "%VSDIR%\Microsoft Visual Studio 10.0\VC\bin\xdcmake.exe"
+call :installSingle "%VSDIR%\Microsoft SDKs\Windows\v7.1\Setup\SDKSetup.exe
 
-gacutil -f -i "%USED_PROGRAM_FILES_32%\MSBuild\Microsoft.Cpp\v4.0\Microsoft.Build.CPPTasks.Common.dll"
-gacutil -f -i "%USED_PROGRAM_FILES_32%\MSBuild\Microsoft.Cpp\v4.0\Platforms\Itanium\Microsoft.Build.CPPTasks.Itanium.dll"
-gacutil -f -i "%USED_PROGRAM_FILES_32%\MSBuild\Microsoft.Cpp\v4.0\Platforms\Win32\Microsoft.Build.CPPTasks.Win32.dll"
-gacutil -f -i "%USED_PROGRAM_FILES_32%\MSBuild\Microsoft.Cpp\v4.0\Platforms\x64\Microsoft.Build.CPPTasks.x64.dll"
-gacutil -f -i "%ProgramFiles%\Reference Assemblies\Microsoft\mmc\v3.0\microsoft.managementconsole.dll"
-gacutil -f -i "%ProgramFiles%\Reference Assemblies\Microsoft\mmc\v3.0\mmcfxcommon.dll"
-gacutil -f -i "%ProgramFiles%\Reference Assemblies\Microsoft\UDDI\v2.1\bin\system32\microsoft.uddi.dll"
-gacutil -f -i "%ProgramFiles%\Reference Assemblies\Microsoft\WindowsMediaServices\v9.6\Microsoft.windowsmediaservices.dll"
-gacutil -f -i "%ProgramFiles%\Reference Assemblies\Microsoft\FSRM\v7.0\srmlib.dll"
+call :installSingle "%USED_PROGRAM_FILES_32%\MSBuild\Microsoft.Cpp\v4.0\Microsoft.Build.CPPTasks.Common.dll"
+call :installSingle "%USED_PROGRAM_FILES_32%\MSBuild\Microsoft.Cpp\v4.0\Platforms\Itanium\Microsoft.Build.CPPTasks.Itanium.dll"
+call :installSingle "%USED_PROGRAM_FILES_32%\MSBuild\Microsoft.Cpp\v4.0\Platforms\Win32\Microsoft.Build.CPPTasks.Win32.dll"
+call :installSingle "%USED_PROGRAM_FILES_32%\MSBuild\Microsoft.Cpp\v4.0\Platforms\x64\Microsoft.Build.CPPTasks.x64.dll"
+call :installSingle "%ProgramFiles%\Reference Assemblies\Microsoft\mmc\v3.0\microsoft.managementconsole.dll"
+call :installSingle "%ProgramFiles%\Reference Assemblies\Microsoft\mmc\v3.0\mmcfxcommon.dll"
+call :installSingle "%ProgramFiles%\Reference Assemblies\Microsoft\UDDI\v2.1\bin\system32\microsoft.uddi.dll"
+call :installSingle "%ProgramFiles%\Reference Assemblies\Microsoft\WindowsMediaServices\v9.6\Microsoft.windowsmediaservices.dll"
+call :installSingle "%ProgramFiles%\Reference Assemblies\Microsoft\FSRM\v7.0\srmlib.dll"
+
+pause
+goto :eof
+:installAll
+for %%i in (*) do (
+echo Install %CD%\%%i
+gacutil -f -i %%i > nul
+)
+goto :eof
+
+goto :eof
+:installSingle
+echo Install %~1
+gacutil -f -i "%~1" > nul
+goto :eof
