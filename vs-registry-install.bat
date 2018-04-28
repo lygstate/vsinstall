1b13be41540a6c63a6c4f966002687b524cf1531
Yonggang Luo <luoyonggang@gmail.com>
2018-04-28 20:06:09 +0800

    Now do not install Visual Studio 2010 & Microsoft SDKs.

--- vs-registry-install.bat
+++ vs-registry-install.bat
@@ -3,13 +3,9 @@ call "%~dp0/prepare.bat"
 
 set VSDIR_REG=%VSDIR:\=\\%
 cscript //Nologo replace.vbs vc6-install.reg.in vc6-install.reg "@VSDIR@" "%VSDIR_REG%"
-cscript //Nologo replace.vbs "vs2010 registry-install.reg.in" "vs2010 registry-install.reg" "@VSDIR@" "%VSDIR_REG%"
-cscript //Nologo replace.vbs "MicrosoftSDKs registry-install.reg.in" "MicrosoftSDKs registry-install.reg" "@VSDIR@" "%VSDIR_REG%"
+cscript //Nologo replace.vbs "visual-studio-registry-install.reg.in" "visual-studio-registry-install.reg" "@VSDIR@" "%VSDIR_REG%"
 
 "%SYSTEM_DIR_32%\regedit.exe" -s vc6-install.reg
-"%SYSTEM_DIR_32%\regedit.exe" -s "vs2010 registry-install.reg"
-
-"%SYSTEM_DIR_32%\regedit.exe" -s "MicrosoftSDKs registry-install.reg"
-regedit.exe -s "MicrosoftSDKs registry-install.reg"
+"%SYSTEM_DIR_32%\regedit.exe" -s "visual-studio-registry-install.reg"
 
 del *.reg
--- vs2010 msbuild-install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "%~dp0/prepare.bat"
-cd /d "%~dp0.."
-xcopy /I /C /E /Y MSBuild "%USED_PROGRAM_FILES_32%\MSBuild"
-xcopy /I /C /E /Y "Reference Assemblies" "%ProgramFiles%\Reference Assemblies"
