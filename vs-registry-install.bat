ee346f833b3d1dae5f8e86b324a4b8971ed359da
Yonggang Luo <luoyonggang@gmail.com>
2018-06-21 11:08:57 +0800

    Fixes Windows SDK Compiler for VS 2010.

--- vs-registry-install.bat
+++ vs-registry-install.bat
@@ -4,9 +4,12 @@ call "%~dp0/prepare.bat"
 set VSDIR_REG=%VSDIR:\=\\%
 cscript //Nologo replace.vbs vc6-install.reg.in vc6-install.reg "@VSDIR@" "%VSDIR_REG%"
 cscript //Nologo replace.vbs "visual-studio-registry-install.reg.in" "visual-studio-registry-install.reg" "@VSDIR@" "%VSDIR_REG%"
+cscript //Nologo replace.vbs "visual-studio-registry-install.x64.reg.in" "visual-studio-registry-install.x64.reg" "@VSDIR@" "%VSDIR_REG%"
 
 "%SYSTEM_DIR_32%\regedit.exe" -s vc6-install.reg
 "%SYSTEM_DIR_32%\regedit.exe" -s "visual-studio-registry-install.reg"
+"%SYSTEM_DIR_32%\regedit.exe" -s "visual-studio-registry-install.x64.reg"
+regedit.exe -s "visual-studio-registry-install.x64.reg"
 
 "%SYSTEM_DIR_32%\regedit.exe" -s "MicrosoftSDKs registry-install.reg"
 regedit.exe -s "MicrosoftSDKs registry-install.reg"
