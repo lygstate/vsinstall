a5638822c38a617a66dddd77791ebfd7206d4f7e
Yonggang Luo <luoyonggang@gmail.com>
2019-10-07 22:39:09 +0800

    Trying to install VS 2010 SDK in a simpler way

--- vs-registry-install.bat
+++ vs-registry-install.bat
@@ -5,6 +5,7 @@ set VSDIR_REG=%VSDIR:\=\\%
 cscript //Nologo replace.vbs vc6-install.reg.in vc6-install.reg "@VSDIR@" "%VSDIR_REG%"
 cscript //Nologo replace.vbs "visual-studio-registry-install.reg.in" "visual-studio-registry-install.reg" "@VSDIR@" "%VSDIR_REG%"
 cscript //Nologo replace.vbs "visual-studio-registry-install.x64.reg.in" "visual-studio-registry-install.x64.reg" "@VSDIR@" "%VSDIR_REG%"
+cscript //Nologo replace.vbs "MicrosoftSDKs registry-install.reg.in" "MicrosoftSDKs registry-install.reg" "@VSDIR@" "%VSDIR_REG%"
 
 "%SYSTEM_DIR_32%\regedit.exe" -s vc6-install.reg
 "%SYSTEM_DIR_32%\regedit.exe" -s "visual-studio-registry-install.reg"
